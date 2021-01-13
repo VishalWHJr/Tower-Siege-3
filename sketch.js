32b3153932d719e7c8930a712a5dac258c37c245
VishalWHJr <73689103+VishalWHJr@users.noreply.github.com>
2021-01-13 14:04:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -205,7 +205,7 @@ function draw(){
     }
 
     async function getTime(){
-        var response=await fetch('http://worldtimeapi.org/api/timezone/Asia/Kolkata');
+        var response=await fetch('https://worldtimeapi.org/api/timezone/Asia/Kolkata');
         var responseJSON=await response.json()
         console.log(responseJSON['datetime']);
         var time=responseJSON['datetime'];
